98178a273d94330a68f0a307ea6bd1df48e7cb8e
Chris Stoute <your-email@example.com>
2026-04-28 09:33:54 -0500

    Added FIM alert logging

--- fim_monitor.ps1
+++ fim_monitor.ps1
@@ -22,11 +22,19 @@ while ($true) {
     Start-Sleep -Seconds 5
 
     $currentHash = (Get-FileHash $file -Algorithm SHA256).Hash
+    $logFile = "fim_log.txt"
+	
 
     if ($currentHash -ne $baselineHash) {
+
+        $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        $logEntry = "$timestamp | ALERT | File modified: $file | Old Hash: $baselineHash | New Hash: $currentHash"
+
         Write-Host "[!!!] WARNING: File has been modified!" -ForegroundColor Red
         Write-Host "Old Hash: $baselineHash"
         Write-Host "New Hash: $currentHash"
+
+        Add-Content -Path $logFile -Value $logEntry
     } else {
         Write-Host "[OK] No changes detected." -ForegroundColor Green
     }
